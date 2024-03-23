7ed4e2a8d7e876279c210610c6fbad524f155596
EngAlaaKamal <147073553+EngAlaaKamal@users.noreply.github.com>
2024-03-23 23:56:51 +0200

    Add files via upload

---
+++ lab4/form/src/App.js
@@ -0,0 +1,14 @@
+import Register from './pages/Register';
+import FormDataPage from './pages/FormDataPage';
+//import './App.css';
+
+function App() {
+  return (
+    <div className="">
+      <Register/>
+      <FormDataPage />
+    </div>
+  );
+}
+
+export default App;
